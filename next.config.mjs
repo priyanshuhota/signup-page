2b48df5b311bc57aaa12789bf3f4a1de0f507db2
priyanshuhota <priyanshuhota.ph@gmail.com>
2024-05-12 21:32:18 +0530

    Update

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
     eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
       ignoreDuringBuilds: true,
     },
   }
 
-export default nextConfig;
+module.exports = nextConfig
